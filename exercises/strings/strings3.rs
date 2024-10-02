e3e1852f828789b596603da42255d3399ebdc6f7
CeS-3 <1540962271@qq.com>
2024-10-02 19:05:55 +0800

    finish strings3

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,23 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let mut new_string = String::from(input);
+    new_string.trim().to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let new_string  = input.to_string() + " world!";
+    new_string
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let new_string = input.to_string().replace("cars", "balloons");
+    new_string
 }
 
 #[cfg(test)]
